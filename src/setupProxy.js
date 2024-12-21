51d740b8c54db819f4982dbb863e9f43c7ebac83
Subho27 <42699151+Subho27@users.noreply.github.com>
2024-12-21 12:11:15 +0530

    last commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
     "/api",
     createProxyMiddleware({
       target: "http://127.0.0.1:8081",
-      changeOrigin: true,
+      changeOrigin: true
     })
   );
 };
